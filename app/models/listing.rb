5abd6f5c33364661f11a45cb4c69a751c3b206e5
Carolyn Nguyen <carolyn.nguyen1@tamu.edu>
2022-02-16 14:47:58 -0600

    added search functionality and cleaned up filtering

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,4 +8,20 @@ class Listing < ApplicationRecord
   validates :rent, presence: true
   has_many :amenities, :through => :amenitymapping
   has_many :users, :through => :favorite
+
+  scope :filter_by_bedroom, -> (type) { where private_bedroom: type == "Private" ? true : false }
+  scope :filter_by_bathroom, -> (type) { where private_bathroom: type == "Private" ? true : false }
+  scope :filter_by_query, -> (query) { where("description iLIKE ? or title iLIKE ?", "%#{query}%", "%#{query}%")}
+
+  def self.filter_by_amenities(amenities)
+    listing_ids = pluck("id")
+    amenities_filtered = AmenityMapping.where(amenity_id: amenities).where(listing_id: listing_ids).group(:listing_id).count
+    official = []
+    amenities_filtered.each do |listing_id, count|
+      if (count == amenities.count)
+        official.push(listing_id)
+      end
+    end
+    where(id: official)
+  end
 end
