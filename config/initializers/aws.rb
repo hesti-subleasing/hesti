7f256ea886c206a356f80ed6cfa8c1ea629aebd9
mrllpz <mrllpz@tamu.edu>
2022-03-10 23:48:40 -0600

    i think it works in dev so theoretically prod

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,12 @@
-require 'aws-sdk-s3'
+# require 'aws-sdk-s3'
 
-Aws.config.update({
-  region: 'us-east-2',
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
-})
+# client = Aws::S3::Client.new(
+#   region: 'us-east-2',
+#   credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+# )
+# # Aws.config.update({
+# #   region: 'us-east-2',
+# #   credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+# # })
 
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['AWS_BUCKET'])
+# S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['AWS_BUCKET'])
